f36ff9cbee274a6c8428aa7dfaf7d54aad13c17b
Nhat-IT <taibanhatk1@gmail.com>
2021-11-12 01:27:29 +0700

    Xong bài 14

---
+++ Code/Bai12,13,14/script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "a9342363da6ea9dc",
+      center: { lat: 15.438406030619612, lng: 108.63799352807553 },
+      zoom: 30,
+    });
+  }
