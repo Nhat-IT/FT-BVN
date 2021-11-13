4a3dcb9ccbedb8276282ffd536889cf7739d2084
Nhat-IT <taibanhatk1@gmail.com>
2021-11-13 12:30:10 +0700

    Xong bài 16

--- Code/Bai16/script.js
+++ Code/Bai16/script.js
@@ -1,9 +1,9 @@
-// var [ten ham] = hàm không tên
+//  var [ten ham] = hàm không tên
 var a = function(){
     console("var [ten ham] = hàm không tên"); 
 }
 
-// var tenham = hàm không tên rút gọn (arrow function)
+//  var tenham = hàm không tên rút gọn (arrow function)
 var b = () => {
     console.log("var tenham = hàm không tên rút gọn (arrow function)");
 }
