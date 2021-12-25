6039dd8ea070041d7cb046b30291475390171157
Nhat-IT <taibanhatk1@.com>
2021-12-25 23:16:22 +0700

    Xong bai 23

--- Code/bai22/src/App.vue
+++ Code/bai22/src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Fullstack 6</h1>
+    <button @click="show = !show">Click</button>
+    <div v-show="show">Nguyen Tan Nhat</div>
+    <div>{{ name }}</div>
+    <input v-model="name " type="text"/>
+    <input type="checkbox" value="hn" v-model="cities">Ha Noi
+    <input type="checkbox" value="dn" v-model="cities"/> Da Nang
+    <input type="checkbox" value="hcm" v-model="cities">Ho Chi Minh
+    <div v-if="cities.length > 0"> Da di {{cities.length}} thanh pho</div>
+    <div v-else>ban chua chon thanh pho</div>
+    <input type="radio" v-model="lecture" value="1"/> math
+    <input type="radio" v-model="lecture" value="2"/> bio
+    <br> 
+    <div v-for="ite in subject" :key="ite.item">
+      <div v-show="!ite.editing">
+        {{ite.item}}
+        <span @click = "ite.editing = true">Sua</span>
+      </div>
+      <div v-show="ite.editing">
+        <input type="text" v-model="ite.item">
+        <button @click = "ite.editing = false">Xong</button>
+      </div>
+    </div>
+    <a :href="link">Link</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      show: true,
+      name: "Nhat",
+      cities: ["hn"],
+      lecture: 2,
+      subject: [
+        {item: "math", editing: false},
+        {item: "art", editing: false},
+        {item: "science", editing: false},
+        {item: "bio", editing: false}
+      ],
+      link: "http://google.com"  
+    }
   }
 }
 </script>
